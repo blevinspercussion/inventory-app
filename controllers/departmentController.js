bd8b004e9597e143341e3fce739d901d4297486e
Adam Blevins <blevinspercussion@gmail.com>
2023-10-13 15:14:05 -0500

    Implemented CRUD functionality for Department

--- controllers/departmentController.js
+++ controllers/departmentController.js
@@ -33,3 +33,152 @@ exports.department_detail = asyncHandler(async (req, res, next) => {
     department_instruments: instrumentsInDepartment,
   });
 });
+
+// Display Department create form on GET
+exports.department_create_get = (req, res, next) => {
+  res.render("department_form", { title: "Create Department" });
+};
+
+// Handle Department create on POST
+exports.department_create_post = [
+  // Validate and sanitize form data
+  body("name")
+    .trim()
+    .isLength({ min: 1 })
+    .escape()
+    .withMessage("Name must be specified.")
+    .isAlphanumeric()
+    .withMessage("Name has non-alphanumeric characters."),
+
+  // Process request after validation and sanitization
+  asyncHandler(async (req, res, next) => {
+    const errors = validationResult(req);
+
+    // Creat Department object with escaped and trimmed data
+    const department = new Department({
+      name: req.body.name,
+    });
+
+    if (!errors.isEmpty) {
+      // There are errors. Render form again with sanitized values/error messages
+      res.render("department_form", {
+        name: "Create Department",
+      });
+      return;
+    } else {
+      // Data form in value
+
+      // Save department
+      await department.save();
+      // Redirect to new department record
+      res.redirect(department.url);
+    }
+  }),
+];
+
+// Display Department delete form on GET
+exports.department_delete_get = asyncHandler(async (req, res, next) => {
+  // Get department and all associated instruments (in parallel)
+  const [department, allInstrumentsInDepartment] = await Promise.all([
+    Department.findById(req.params.id).exec(),
+    Instrument.find({ department: req.params.id }, "name description").exec(),
+  ]);
+
+  if (department === null) {
+    res.redirect("/catalog/departments");
+  }
+
+  res.render("department_delete", {
+    title: "Delete Department",
+    department: department,
+    department_instruments: allInstrumentsInDepartment,
+  });
+});
+
+// Handle Department delete on POST
+exports.department_delete_post = asyncHandler(async (req, res, next) => {
+  // Get details of departmest and all associated books (in paraller)
+  const [department, allInstrumentsInDepartment] = await Promise.all([
+    Department.findById(req.params.id).exec(),
+    Instrument.find({ department: req.params.id }, "name description").exec(),
+  ]);
+
+  if (allInstrumentsInDepartment > 0) {
+    // Department has instruments. Render in same way as GET route
+    res.render("department_delete", {
+      title: "Delete Department",
+      department: department,
+      department_instruments: allInstrumentsInDepartment,
+    });
+    return;
+  } else {
+    // Department has no instruments. Delete object and redirect to the list of departments
+    await Department.findByIdAndRemove(req.body.departmentid);
+    res.redirect("/catalog/departments");
+  }
+});
+
+// Display Department update form on GET
+exports.department_update_get = asyncHandler(async (req, res, next) => {
+  // Get department, instruments, and manufacturers for form
+  const [department, allManufacturers, allInstruments] = await Promise.all([
+    Department.findById(req.params.id).exec(),
+    Manufacturer.find().exec(),
+    Instrument.find().exec(),
+  ]);
+
+  if (department === null) {
+    const err = new Error("Department not found");
+    err.status = 404;
+    return next(err);
+  }
+
+  res.render("department_form", {
+    title: "Update Department",
+    instruments: allInstruments,
+    manufacturers: allManufacturers,
+    department: department,
+  });
+});
+
+// Handle Department update on POST
+exports.department_update_post = [
+  // Validate and sanitize fields
+  body("name", "Name must not be empty.").trim().isLength({ min: 1 }).escape(),
+
+  // Process request after validation and sanitization
+  asyncHandler(async (req, res, next) => {
+    // Extract the validation errors from a request
+    const errors = validationResult(req);
+
+    // Create a Department object with escaped/trimmed data and old id
+    const department = new Department({
+      name: req.body.name,
+      _id: req.params.id, // This is required or a new ID will be assigned!
+    });
+
+    if (!errors.isEmpty()) {
+      const [allManufacturers, allInstruments] = await Promise.all([
+        Manufacturer.find().exec(),
+        Instrument.find().exec(),
+      ]);
+
+      res.render("department_form", {
+        title: "Update Department",
+        departments: allDepartments,
+        instruments: allInstruments,
+        manufacturers: allManufacturers,
+        errors: errors.array(),
+      });
+      return;
+    } else {
+      // data from form is valid. Update the recond
+      const updatedDepartment = await Department.findByIdAndUpdate(
+        req.params.id,
+        department,
+        {}
+      );
+      res.redirect(updatedDepartment.url);
+    }
+  }),
+];
--- routes/catalog.js
+++ routes/catalog.js
@@ -5,17 +5,48 @@ const router = express.Router();
 const department_controller = require("../controllers/departmentController");
 const manufacturer_controller = require("../controllers/manufacturerController");
 const instrument_controller = require("../controllers/instrumentController");
+const department = require("../models/department");
 
 /// DEPARTMENT ROUTES ///
 
 // GET catalog home page
 router.get("/", instrument_controller.index);
 
+// GET request for creating a Department. NOTE this must come before routes that display Book (uses ID)
+router.get("/department/create", department_controller.department_create_get);
+
+// POST request for creating a department
+router.post("/department/create", department_controller.department_create_post);
+
 // GET request for one department
 router.get("/department/:id", department_controller.department_detail);
 
 // GET request for all departments
 router.get("/departments", department_controller.department_list);
+
+// GET request to delete a department
+router.get(
+  "/department/:id/delete",
+  department_controller.department_delete_get
+);
+
+// POST request to delete a department
+router.post(
+  "/department/:id/delete",
+  department_controller.department_delete_post
+);
+
+// GET request to update a department
+router.get(
+  "/department/:id/update",
+  department_controller.department_update_get
+);
+
+// POST request to upadate a department
+router.post(
+  "/department/:id/update",
+  department_controller.department_update_post
+);
 
 /// MANUFACTURER ROUTES ///
 
